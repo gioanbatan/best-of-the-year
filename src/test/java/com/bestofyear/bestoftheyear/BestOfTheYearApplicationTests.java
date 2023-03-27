b1bec29e553e221854e873e39a5bce07b9d258d7
gioanbatan <giovannibassani@hotmail.com>
2023-03-27 14:24:15 +0200

    first commit

---
+++ src/test/java/com/bestofyear/bestoftheyear/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestofyear.bestoftheyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
